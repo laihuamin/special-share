29b284c77af258aae2049ca880eee686f9a7ffd3
laihuamin <1445509994@qq.com>
2018-02-27 10:00:39 +0800

    runtime

--- babel/lib/assign.js
+++ babel/lib/assign.js
@@ -1,7 +1,13 @@
 "use strict";
 
+var _assign = require("babel-runtime/core-js/object/assign");
+
+var _assign2 = _interopRequireDefault(_assign);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 var a = {
     b: 1
 };
 
-var c = Object.assign({}, a);
+var c = (0, _assign2.default)({}, a);
