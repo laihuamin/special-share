47c65ac25f38f4e072584526a82741367d949de8
laihuamin <1445509994@qq.com>
2018-03-07 23:05:19 +0800

    babel

---
+++ babel/example/assignpolyfill.js
@@ -0,0 +1,9 @@
+require('babel-polyfill');
+
+Object.assign({}, {});
+Object.assign({}, {});
+Object.assign({}, {});
+Object.assign({}, {});
+Object.assign({}, {});
+Object.assign({}, {});
+Object.assign({}, {});
